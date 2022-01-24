544ca5c56a84bd1c8fd52ce821abd6f7a534d3b5
Hamza Khalid <hk952078@gmail.com>
2022-01-24 10:57:01 +0500

    initial commit

--- app/src/main/java/com/example/naunehalmidline/SecondActivity.kt
+++ app/src/main/java/com/example/naunehalmidline/SecondActivity.kt
@@ -2,8 +2,14 @@ package com.example.naunehalmidline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import com.example.naunehalmidline.databinding.ActivitySecondBinding
+import com.validatorcrawler.aliazaz.Clear
+import kotlinx.coroutines.launch
 
 class SecondActivity : AppCompatActivity() {
 
@@ -21,5 +27,34 @@ class SecondActivity : AppCompatActivity() {
 
 
         }*/
+        binding.btnEnd.setOnClickListener {
+            createDialog()
+
+
+        }
     }
+
+    private fun createDialog() {
+        AlertDialog.Builder(this@MainActivity).apply {
+            setCancelable(true)
+            setTitle("Alert Dialog")
+            setMessage("Are you sure you want to clear this form?")
+            setPositiveButton("Yes") { dialog, id ->
+                //Action for "Delete".
+                Log.e("click", "Positive")
+
+                lifecycleScope.launch {
+                    val def = launch { Clear.clearAllFields(binding.GrpName) }
+                    def.join()
+                    /*startActivity(Intent(this@MainActivity, MainActivity::class.java))*/
+                    binding.Grp.fullScroll(View.FOCUS_UP)
+                }
+            }
+            setNegativeButton("Cancel") { dialog, which ->
+                //Action for "Cancel".
+                Log.e("click", "Negative")
+            }
+        }.create().show()
+    }
+
 }
