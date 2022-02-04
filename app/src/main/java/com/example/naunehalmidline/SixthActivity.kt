0523747d40f8ea0760892947c4bcc806ce01ab55
Hamza Khalid <hk952078@gmail.com>
2022-02-03 16:34:03 -0800

    *commit

--- app/src/main/java/com/example/naunehalmidline/SixthActivity.kt
+++ app/src/main/java/com/example/naunehalmidline/SixthActivity.kt
@@ -2,10 +2,14 @@ package com.example.naunehalmidline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.naunehalmidline.databinding.ActivitySixthBinding
 
 class SixthActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySixthBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sixth)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sixth)
+        binding.callback
     }
 }
